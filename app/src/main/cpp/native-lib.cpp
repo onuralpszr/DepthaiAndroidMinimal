4ff4f3510a4b0bd5473b4093645620beba3b1a58
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2022-08-28 21:20:16 +0300

    gradle update and get usb speed to show minor start
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,20 +4,23 @@
 #include <libusb.h>
 #include "depthai/depthai.hpp"
 
-extern "C" JNIEXPORT jstring JNICALL
+extern "C" JNIEXPORT jstring
 Java_com_example_depthaiandroid_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     auto r = libusb_set_option(nullptr, LIBUSB_OPTION_ANDROID_JNIENV, env);
 
     // Connect to device and start pipeline
-    auto device = dai::Device(dai::OpenVINO::VERSION_2021_4, dai::UsbSpeed::HIGH);
+    auto device = dai::Device(dai::OpenVINO::VERSION_2021_4, dai::UsbSpeed::SUPER);
 
     // Create pipeline
     dai::Pipeline pipeline;
+
     device.startPipeline(pipeline);
 
+    //enum class UsbSpeed : int32_t { UNKNOWN, LOW, FULL, HIGH, SUPER, SUPER_PLUS };
 
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    auto usbVal = static_cast<int32_t>(device.getUsbSpeed());
+    std::string usbSpeedText = "DepthAi UsbSpeed: "+std::to_string(usbVal);
+    return env->NewStringUTF(usbSpeedText.c_str());
 }
